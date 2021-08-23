40d2fab62224642b6f0afcb1a0003030770a46ff
Exequiel Rozas <exequiel98@gmail.com>
2021-08-23 12:27:42 -0400

    Add 'remember_me' expectation

--- spec/kraken_invoicing_spec.rb
+++ spec/kraken_invoicing_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe KrakenInvoicing do
       config.api_endpoint = "https://kraken-endpoint.com/api"
       config.client_id = "prettyFakeClientId"
       config.client_secret = "prettyFakeClientSecret"
+      config.remember_me = true
     end
   end
 
@@ -19,5 +20,6 @@ RSpec.describe KrakenInvoicing do
     expect(KrakenInvoicing.configuration.api_endpoint).to eq("https://kraken-endpoint.com/api")
     expect(KrakenInvoicing.configuration.client_id).to eq("prettyFakeClientId")
     expect(KrakenInvoicing.configuration.client_secret).to eq("prettyFakeClientSecret")
+    expect(KrakenInvoicing.configuration.remember_me).to eq(true)
   end
 end
